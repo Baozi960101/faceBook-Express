10bfac4945894aa6acf45704480121d6deb6f954
Baozi960101 <Baozi960101@gmail.com>
2022-10-10 23:59:42 +0800

    1010

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -8,6 +8,7 @@ const secretKey = "thisismynewproject";
 
 const usersController = {
   getAll: (req, res) => {
+    res.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate")
     Comment.findAll({
       include: {
         model: User,
